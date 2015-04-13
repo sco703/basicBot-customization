cf4e54f956ddf15072b53d1269ea68a23567d9c0
sco703 <scohen703@gmail.com>
2015-04-12 22:09:05 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -75,7 +75,7 @@
         voteSkip: false,
         voteSkipLimit: 10,
         timeGuard: true,
-        maximumSongLength: 10,
+        maximumSongLength: 80,
         autodisable: true,
         commandCooldown: 30,
         usercommandsEnabled: true,
